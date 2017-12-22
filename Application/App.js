8353e9f3955ec2e5ddbe6667b64c385a8b57011b
Abhishek Velayudham <avelay@terpmail.umd.edu>
2017-12-22 01:11:28 -0500

    first

---
+++ Application/App.js
@@ -0,0 +1,11 @@
+import React from 'react';
+// import { Text } from 'react-native';
+import Header from './components/header';
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <Header headerText={'MusiDB'} />
+    );
+  }
+}
